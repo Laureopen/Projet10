499f92cb9d5127c03143b7fc7d491266e2739838
laurence <laur.grenouille@orange.fr>
2025-05-23 11:08:54 +0200

    Mise à jour

--- softdesk/api/admin.py
+++ softdesk/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Project, Contributor, Issue
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Contributor)
+admin.site.register(Issue)
